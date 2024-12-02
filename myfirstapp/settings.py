b525b716cb5e0a9723d2eb93a60e34353d56f95b
Olaoye kunle <51406569+kunle1984@users.noreply.github.com>
2024-12-02 13:32:39 +0100

    set all host

--- myfirstapp/settings.py
+++ myfirstapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-lloz#$!cirhmmxz5#m9oi==+vawq1)+uqvqf(j+09#+=b_e73c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
